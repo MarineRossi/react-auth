d555a90f36c3b88faf1a3e5c2932a90ae6891f46
Marine ROSSI <marine.rossi11@gmail.com>
2023-12-04 17:00:11 +0100

    changing name folder

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,97 @@
+// src/pages/LoginPage.jsx
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import authService from "../services/auth.service";
+
+const API_URL = "http://localhost:5005";
+
+function LoginPage(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const { storeToken } = useContext(AuthContext);
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          const user = response.data;
+
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(user);
+        })
+        .catch((error) => {
+          setIsLoggedIn(false);
+          setIsLoading(false);
+          setUser(null);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {}, []);
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+
+   
+    authService.login(requestBody) 
+      .then((response) => {
+        console.log("JWT token", response.data.authToken);
+        storeToken(response.data.authToken);
+        authenticateUser();
+        navigate("/");
+      })
+      .catch((error) => {
+      	const errorDescription = error.response.data.message;
+      	setErrorMessage(errorDescription);
+    	})
+  };
+ 
+
+
+  return (
+    <div className="LoginPage">
+      <h1>Login</h1>
+
+      <form onSubmit={handleLoginSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Don't have an account yet?</p>
+      <Link to={"/signup"}> Sign Up</Link>
+    </div>
+  );
+}
+
+export default LoginPage;
